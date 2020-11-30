50e2de7b387dbe3b75e50f7bcfdc09ac13eb61c1
Sravan <sravan5432@gmail.com>
2020-11-30 22:41:55 +0530

    fas

--- Lab02/backend.tf
+++ Lab02/backend.tf
@@ -1,7 +1,3 @@
 terraform {
   backend "azurerm" {}
-  resource_group_name  = "Terra-rg"
-  storage_account_name = "newremotestate"
-  container_name       = "statefiles"
-  key                  = "test.tfstate"
 }
